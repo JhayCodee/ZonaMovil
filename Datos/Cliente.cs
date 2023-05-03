637fd2c89894b2328380e2a6b25536a50234b98f
Luis Vargas <kluis0178@gmail.com>
2023-05-02 23:45:37 -0600

    se actualizo la tabla cliente

--- Datos/Cliente.cs
+++ Datos/Cliente.cs
@@ -27,6 +27,7 @@ namespace Datos
         public string Cedula { get; set; }
         public string Correo { get; set; }
         public string Telefono { get; set; }
+        public int activo { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<FacturaVenta> FacturaVenta { get; set; }
