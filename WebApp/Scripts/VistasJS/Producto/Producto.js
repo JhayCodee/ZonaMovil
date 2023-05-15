24599ec6400afa50e86f9d98f233f1d3cf0b5a22
Luis Vargas <kluis0178@gmail.com>
2023-05-15 00:34:15 -0600

    Vista de producto y cargar al editar + select pickers

--- WebApp/Scripts/VistasJS/Producto/Producto.js
+++ WebApp/Scripts/VistasJS/Producto/Producto.js
@@ -12,57 +12,282 @@
 
     // se oculta el formulario
     vistaProductoComponente.contenedorFormulario.hide();
+    cargarTablaProductos();
 
-    // se hace una peticion de los clientes para cargar la data table (la tabla de jquery)
-    $.ajax({
-        url: vistaProductoComponente.url + "/ListarProductos",
-        type: "GET",
-        success: function (data) {
-            tablaProducto = $("#tablaProducto").DataTable({
-                "data": data,
-                dom: "<'row'<'col-md-12'B>>" + "<'row'<'col-md-6'l><'col-md-6'f>>" + "<'row'<'col-md-12'tr>>" + "<'row'<'col-md-5'i><'col-md-7'p>>",
-                lengthMenu: [[10, 25, 50, -1], [10, 25, 50, "TODOS"]],
-                pageLength: 10,
-                "columns": [
-                    { "data": "Nombre" },
-                    { "data": "Modelo" },
-                    { "data": "Stock" },
-                    { "data": "GarantiaEnMeses" },
-                    { "data": "PrecioCompra" },
-                    { "data": "PrecioVenta" },
-                    {
-                        "data": null,
-                        "render": function (data, type, row, meta) {
-                            return '<div class="centrar" style="display:flex; justify-content:center; gap:6px;">' +
-                                '<button type="button" class="btn btn-info btnVer" data-id="' + row.IdCliente + '"> <i class="fas fa-solid fa-eye"></i> </button>' +
-                                '<button type="button" class="btn btn-primary btnEditar" data-id="' + row.IdCliente + '"> <i class="fas fa-solid fa-pen"></i> </button>' +
-                                '<button type="button" class="btn btn-danger btnEliminar" data-id="' + row.IdCliente + '"> <i class="fas fa-solid fa-trash"></i> </button>' +
-                                '</div>';
-                        }
-                    }
+    // poblar el formularion con editar:
+    $("#tablaProducto").on('click', '.btnEditar', function () {
+        // Obtener el ID del cliente
+        var producto = $(this).data('id');
+        modo = "editar"
+        resetFormProduct();
 
-                ],
-                buttons: [
-                    {
-                        text: 'Nuevo',
-                        className: 'btn btn-primary mb-2',
-                        titleAttr: "Crear un nuevo registro",
-                        // si se da click en el boton mostrar el formulario y ocultar la tabla
-                        action: function (e, dt, node, config) {
-                            $('#FormularioCliente')[0].reset();
-                            modo = "crear"
-                            //vistaClienteComponente.contenedorTabla.hide();
-                            //vistaClienteComponente.contenedorFormulario.show();
-                        }
+        // Llamar al método de búsqueda por ID para obtener la información del cliente
+        $.ajax({
+            url: vistaProductoComponente.url + '/BuscarProductoPorID',
+            type: 'POST',
+            data: { id: producto },
+            success: function (producto) {
+                // Poblar el formulario con la información del cliente
+                $('#NombreProducto').val(producto.Nombre);
+                $('#ModeloProducto').val(producto.Modelo);
+                $('#StockProducto').val(producto.Stock);
+                $('#GarantiaProducto').val(producto.GarantiaEnMeses);
+                $('#PrecioCompra').val(producto.PrecioCompra);
+                $('#PrecioVenta').val(producto.PrecioVenta);
+                $('#DescripcionProducto').val(producto.Descripcion);
 
-                    },
-                ]
-            });
-        },
-        error: function () {
-            console.log("Error al obtener los clientes.");
-        }
+                //poblar los selectpicker
+                cargarOpcionesCategoria();
+                cargarOpcionesMarca();
+
+                // Seleccionar la opción correspondiente al ID de categoría del producto
+                $('#categoriaSelect').val(producto.IdCategoria);
+                // Actualizar el selectpicker de categoría
+                $('#categoriaSelect').selectpicker('refresh');
+
+                // Seleccionar la opción correspondiente al ID de marca del producto
+                $('#MarcaSelect').val(producto.IdMarca);
+                // Actualizar el selectpicker de marca
+                $('#MarcaSelect').selectpicker('refresh');
+
+                // Mostrar el formulario
+                vistaProductoComponente.contenedorTabla.hide();
+                vistaProductoComponente.contenedorFormulario.show();
+            },
+            error: function () {
+                console.log('Error al obtener la información del cliente.');
+            }
+        });
     });
 
+    $('#btnGuardarProducto').click(function (e) {
+
+        e.preventDefault();
+
+        if ($('#FormularioProducto').valid()) {
+            console.log('Valido');
+
+            var data = {
+                Nombre: $('#NombreProducto').val(),
+                Modelo: $('#ModeloProducto').val(),
+                Stock: $('#StockProducto').val(),
+                GarantiaEnMeses: $('#GarantiaProducto').val(),
+                IdMarca: $('#MarcaIdInput').val(),
+                IdCategoria: $('#categoriaIdInput').val(),
+                PrecioCompra: $('#PrecioCompra').val(),
+                PrecioVenta: $('#PrecioVenta').val(),
+                Descripcion: $('#DescripcionProducto').val(),
+                Activo: true
+            };
+
+            $.ajax({
+                type: 'POST',
+                url: '/Producto/AgregarProducto',
+                data: data,
+                success: function (response) {
+                    // Manejar la respuesta del controlador si es necesario
+                    console.log(response);
+                    // Mostrar SweetAlert de éxito y volver a la tabla
+                    Swal.fire(
+                        'Agregado!',
+                        'Este Producto se ha Agregado.',
+                        'success'
+                    ).then(() => {
+                        cargarTablaProductos();
+                        resetFormProduct();
+                        vistaProductoComponente.contenedorTabla.show();
+                        vistaProductoComponente.contenedorFormulario.hide();
+                    });
+                },      
+                error: function (xhr, status, error) {
+                    // Manejar el error si es necesario
+                    console.log(xhr.responseText);
+                }
+            });
+
+        }
+        else {
+            console.log('Invalido');
+        }
+
+    });
+    
+
+
+    // Listener para el evento "changed.bs.select" del selectpicker categoria
+    $('#categoriaSelect').on('changed.bs.select', function (e, clickedIndex, isSelected, previousValue) {
+        // Obtener ID de la categoría seleccionada
+        var categoriaId = $(this).val();
+
+        // Establecer valor del input oculto
+        $('#categoriaIdInput').val(categoriaId);
+    });
+
+    // Listener para el evento "changed.bs.select" del selectpicker marca
+    $('#MarcaSelect').on('changed.bs.select', function (e, clickedIndex, isSelected, previousValue) {
+        // Obtener ID de la categoría seleccionada
+        var MarcaId = $(this).val();
+
+        // Establecer valor del input oculto
+        $('#MarcaIdInput').val(MarcaId);
+    });
+
+    function cargarTablaProductos() {
+        $.ajax({
+            url: "/Producto/ListarProductos",
+            type: "GET",
+            success: function (data) {
+                if ($.fn.DataTable.isDataTable('#tablaProducto')) {
+                    $('#tablaProducto').DataTable().destroy();
+                }
+                tablaProducto = $("#tablaProducto").DataTable({
+                    "data": data,
+                    dom: "<'row'<'col-md-12'B>>" + "<'row'<'col-md-6'l><'col-md-6'f>>" + "<'row'<'col-md-12'tr>>" + "<'row'<'col-md-5'i><'col-md-7'p>>",
+                    lengthMenu: [[10, 25, 50, -1], [10, 25, 50, "TODOS"]],
+                    pageLength: 10,
+                    "columns": [
+                        { "data": "Nombre" },
+                        { "data": "Modelo" },
+                        { "data": "Stock" },
+                        { "data": "GarantiaEnMeses" },
+                        { "data": "PrecioCompra" },
+                        { "data": "PrecioVenta" },
+                        {
+                            "data": null,
+                            "render": function (data, type, row, meta) {
+                                return '<div class="centrar" style="display:flex; justify-content:center; gap:6px;">' +
+                                    '<button type="button" class="btn btn-info btnVer" data-id="' + row.IdProducto + '"> <i class="fas fa-solid fa-eye"></i> </button>' +
+                                    '<button type="button" class="btn btn-primary btnEditar" data-id="' + row.IdProducto + '"> <i class="fas fa-solid fa-pen"></i> </button>' +
+                                    '<button type="button" class="btn btn-danger btnEliminar" data-id="' + row.IdProducto + '"> <i class="fas fa-solid fa-trash"></i> </button>' +
+                                    '</div>';
+                            }
+                        }
+
+                    ],
+                    buttons: [
+                        {
+                            text: 'Nuevo',
+                            className: 'btn btn-primary mb-2',
+                            titleAttr: "Crear un nuevo registro",
+                            // si se da click en el boton mostrar el formulario y ocultar la tabla
+                            action: function (e, dt, node, config) {
+                                resetFormProduct();
+                                modo = "crear"
+                                cargarOpcionesCategoria();
+                                cargarOpcionesMarca();
+                                vistaProductoComponente.contenedorTabla.hide();
+                                vistaProductoComponente.contenedorFormulario.show();
+                            }
+
+                        },
+                        {
+                            extend: 'excel',
+                            text: 'Exportar a Excel',
+                            className: 'btn btn-success mb-2',
+                            titleAttr: "Exportar a Excel"
+                        },
+                        {
+                            extend: 'pdf',
+                            text: 'Exportar a PDF',
+                            className: 'btn btn-danger mb-2',
+                            titleAttr: "Exportar a PDF"
+                        },
+                        {
+                            extend: 'csv',
+                            text: 'Exportar a CSV',
+                            className: 'btn btn-warning mb-2',
+                            titleAttr: "Exportar a CSV"
+                        },
+                        {
+                            extend: 'print',
+                            text: 'Imprimir',
+                            className: 'btn btn-info mb-2',
+                            titleAttr: "Imprimir"
+                        },
+                    ]
+                });
+            },
+            error: function () {
+                console.log("Error al obtener los clientes.");
+            }
+        });
+    }
+
+    // Función para cargar opciones del selectpicker
+    function cargarOpcionesCategoria() {
+        $.ajax({
+            url: '/Categoria/ListarCategorias',
+            type: 'GET',
+            dataType: 'json',
+            success: function (data) {
+                // Obtener el selectpicker
+                var selectpicker = $('#categoriaSelect');
+
+                // Obtener la opción "Seleccionar categoría"
+                var opcionSeleccionar = selectpicker.find('option[value=""]');
+
+                // Deshabilitar la opción "Seleccionar categoría"
+                opcionSeleccionar.prop('disabled', true);
+
+                // Agregar opciones al selectpicker
+                $.each(data, function (key, value) {
+                    selectpicker.append('<option value="' + value.IdCategoria + '">' + value.Nombre + '</option>');
+                });
+
+                // Actualizar selectpicker
+                selectpicker.selectpicker('refresh');
+            },
+            error: function (xhr, status, error) {
+                // Manejar errores
+                console.log(error);
+            }
+        });
+    }
+
+    // Función para cargar opciones del selectpicker
+    function cargarOpcionesMarca() {
+        $.ajax({
+            url: '/Marca/ListarMarcas',
+            type: 'GET',
+            dataType: 'json',
+            success: function (data) {
+                // Obtener el selectpicker
+                var selectpicker = $('#MarcaSelect');
+
+                // Obtener la opción "Seleccionar categoría"
+                var opcionSeleccionar = selectpicker.find('option[value=""]');
+
+                // Deshabilitar la opción "Seleccionar categoría"
+                opcionSeleccionar.prop('disabled', true);
+
+                // Agregar opciones al selectpicker
+                $.each(data, function (key, value) {
+                    selectpicker.append('<option value="' + value.IdMarca + '">' + value.Nombre + '</option>');
+                });
+
+                // Actualizar selectpicker
+                selectpicker.selectpicker('refresh');
+            },
+            error: function (xhr, status, error) {
+                // Manejar errores
+                console.log(error);
+            }
+        });
+    }
+
+    function resetFormProduct() {
+        $("#FormularioProducto")[0].reset();
+        $("#FormularioProducto").validate().resetForm();
+        $('#MarcaSelect').val('').selectpicker('refresh');
+        $('#categoriaSelect').val('').selectpicker('refresh');
+        $("#FormularioProducto").find(".is-invalid").removeClass("is-invalid");
+        $("#FormularioProducto").find(".is-valid").removeClass("is-valid");
+    }
+
+    $('#btnRegresarProducto').click(function () {
+        resetFormProduct();
+        vistaProductoComponente.contenedorTabla.show();
+        vistaProductoComponente.contenedorFormulario.hide();
+    });
 
 });
---
+++ WebApp/Scripts/VistasJS/Producto/Validaciones.js
@@ -0,0 +1,105 @@
+﻿$(document).ready(function () {
+    // Agregar reglas de validación requerida
+    $("#FormularioProducto").validate({
+        rules: {
+            Nombre: {
+                required: true
+            },
+            Modelo: {
+                required: true
+            },
+            Stock: {
+                required: true
+            },
+            GarantiaEnMeses: {
+                required: true
+            },
+            IdMarca: {
+                required: true
+            },
+            IdCategoria: {
+                required: true
+            },
+            PrecioCompra: {
+                required: true
+            },
+            PrecioVenta: {
+                required: true
+            },
+            Descripcion: {
+                required: true
+            }
+        },
+        messages: {
+            Nombre: {
+                required: "Ingrese el nombre del producto"
+            },
+            Modelo: {
+                required: "Ingrese el modelo del producto"
+            },
+            Stock: {
+                required: "Ingrese la cantidad en stock"
+            },
+            GarantiaEnMeses: {
+                required: "Ingrese el periodo de garantía"
+            },
+            IdMarca: {
+                required: "Seleccione una marca"
+            },
+            IdCategoria: {
+                required: "Seleccione una categoría"
+            },
+            PrecioCompra: {
+                required: "Ingrese el precio de compra"
+            },
+            PrecioVenta: {
+                required: "Ingrese el precio de venta"
+            },
+            Descripcion: {
+                required: "Ingrese una descripción"
+            }
+        },
+        errorElement: "span",
+        highlight: function (element, errorClass, validClass) {
+            if ($(element).hasClass("selectpicker")) {
+                $(element)
+                    .closest(".form-group")
+                    .addClass("is-invalid")
+                    .removeClass("is-valid")
+                    .find("button.dropdown-toggle")
+                    .addClass("is-invalid");
+            } else {
+                $(element).addClass("is-invalid").removeClass("is-valid");
+            }
+        },
+
+        // Función para quitar el resaltado del contenedor del selectpicker
+        unhighlight: function (element, errorClass, validClass) {
+            if ($(element).hasClass("selectpicker")) {
+                $(element)
+                    .closest(".form-group")
+                    .addClass("is-valid")
+                    .removeClass("is-invalid")
+                    .find("button.dropdown-toggle")
+                    .removeClass("is-invalid");
+            } else {
+                $(element).addClass("is-valid").removeClass("is-invalid");
+            }
+        },
+
+        // Función para mostrar los mensajes de error del selectpicker
+        errorPlacement: function (error, element) {
+            if ($(element).hasClass("selectpicker")) {
+                element.closest(".form-group").append(error);
+            } else {
+                error.insertAfter(element);
+            }
+        }
+    });
+
+    // Manejar el evento change de los selectpicker para ocultar los mensajes de error
+    $('#MarcaSelect, #categoriaSelect').on('changed.bs.select', function () {
+        $(this).valid();
+    });
+});
+
